8311a687b969637364e1572f58a81f9496d50f27
Isaac Suttell <isaac.suttell@sony.com>
2018-09-07 09:30:09 -0700

    added ceres for logging

--- node_core_logger.js
+++ node_core_logger.js
@@ -1,53 +1,26 @@
-const chalk = require('chalk');
+const Ceres = require('ceres-framework');
 
-LOG_TYPES = {
-  NONE: 0,
-  ERROR: 1,
-  NORMAL: 2,
-  DEBUG: 3,
-  FFDEBUG: 4
-};
-
-let logType = LOG_TYPES.NORMAL;
-
-const setLogType = (type) => {
-  if (typeof type !== 'number') return;
-
-  logType = type;
-};
-
-const logTime = () => {
-  let nowDate = new Date();
-  return nowDate.toLocaleDateString() + ' ' + nowDate.toLocaleTimeString([], { hour12: false });
-};
-
-const log = (...args) => {
-  if (logType < LOG_TYPES.NORMAL) return;
-
-  console.log(logTime(), process.pid, chalk.bold.green('[INFO]'), ...args);
-};
-
-const error = (...args) => {
-  if (logType < LOG_TYPES.ERROR) return;
-
-  console.log(logTime(), process.pid, chalk.bold.red('[ERROR]'), ...args);
-};
-
-const debug = (...args) => {
-  if (logType < LOG_TYPES.DEBUG) return;
-
-  console.log(logTime(), process.pid, chalk.bold.blue('[DEBUG]'), ...args);
-};
-
-const ffdebug = (...args) => {
-  if (logType < LOG_TYPES.FFDEBUG) return;
-
-  console.log(logTime(), process.pid, chalk.bold.blue('[FFDEBUG]'), ...args);
-};
+const logger = Ceres.logger('NodeMediaServer');
 
 module.exports = {
-  LOG_TYPES,
-  setLogType,
-
-  log, error, debug, ffdebug
+  LOG_TYPES: {
+    NONE: 0,
+    ERROR: 1,
+    NORMAL: 2,
+    DEBUG: 3,
+    FFDEBUG: 4
+  },
+  setLogType: () => undefined,
+  log: (...args) => {
+    logger.info(...args);
+  },
+  error: (...args) => {
+    logger.error(...args);
+  },
+  debug: (...args) => {
+    logger.debug(args);
+  },
+  ffdebug: (...args) => {
+    logger.debug(args);
+  }
 }
